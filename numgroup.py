1d00de5e7b0d4cd9f748a7078c50d92571552ba4
craftJ <whiteday_dreamer@163.com>
2018-04-27 00:40:00 +0800

    递归实现全排列

--- numgroup.py
+++ numgroup.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+# coding:utf-8
+
+#需要优化，输入数字或者是字符串，都应该可以搞得定，类似于排列组合的一个东西
+
 num = 0
 for i in range(1,5):
     for j in range(1,5):
@@ -7,9 +11,33 @@ for i in range(1,5):
             #print(i,j,k)
             if(i != j) and (i != k) and (j != k):
                 num+=1
-                print(i,j,k)
+#                print(i,j,k)
 
-print(num)
+#print(num)
+
+
+#索引的使用，左侧包含边界，右侧不包含边界，-1则是从最右侧的元素开始
+
+def perms(elements):
+    if len(elements) <= 1:
+        yield elements
+    else:
+        for perm in perms(elements[1:]):
+           # print("aaaaa",perm)
+           for i in range(len(elements)):
+               yield perm[:i] + elements[0:1] + perm[i:]
+
+for items in list(perms([1,2,3,4])):
+    print(items)
+    
+
+
+
+
+
+
+
+
 
 
 
