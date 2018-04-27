8785fd618c6ffe496750a309b9e5d37b78ada601
craftJ <whiteday_dreamer@163.com>
2018-04-28 00:23:20 +0800

    全排列算法分析，标注思路

--- numgroup.py
+++ numgroup.py
@@ -18,6 +18,10 @@ for i in range(1,5):
 
 #索引的使用，左侧包含边界，右侧不包含边界，-1则是从最右侧的元素开始
 
+
+#给定4个数全排列的递归算法实现，
+#4个数全排列其实等于3个数全排列的可能性再加上1个数插到所有位置，
+#依次类推，运用yield的记忆属性，可以实现全排列收集
 def perms(elements):
     if len(elements) <= 1:
         yield elements
@@ -25,6 +29,7 @@ def perms(elements):
         for perm in perms(elements[1:]):
            # print("aaaaa",perm)
            for i in range(len(elements)):
+               #print("bbbbbb",i,perm[:i],elements[0:1],perm[i:])
                yield perm[:i] + elements[0:1] + perm[i:]
 
 for items in list(perms([1,2,3,4])):
